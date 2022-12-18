979931b2724d8984ca27603118494623ed53b63a
Alexander King <brooksgate@Alexanders-MBP.hitronhub.home>
2022-12-17 21:06:50 -0600

    updated

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -61,7 +61,7 @@ resource "aws_iam_instance_profile" "instance_profile" {
 
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0b0dcb5067f052a63"
+  ami                    = "ami-0b5eea76982371e91"
   instance_type          = "t2.large"
   key_name               = "jenkins"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
